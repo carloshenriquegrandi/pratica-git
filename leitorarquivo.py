149588dc9fedabfe64dd25e5b5843f9e448fd5dd
carloshenriquegrandi <carloshenriquegrandi2@gamil.com>
2024-03-15 08:56:03 -0300

    Classe para leitura dos dados a partir do arquivo

---
+++ leitorarquivo.py
@@ -0,0 +1,17 @@
+
+class leitorarquivo:
+    def __init__(self, nomeArquivo):
+        self.arq = open(nomeArquivo, 'r')
+        self.valores = [float(x) for x in self.arq.readline().split()]
+   
+    def getValores(self):
+        return self.valores
+
+
+
+if __name__ == '__main__':
+    leitor = leitorarquivo("data.txt")
+    listaValores = leitor.getValores()
+    print(listaValores)
+
+
