9f7680358ed9321320f64f3a1510a7321d3c8e86
carloshenriquegrandi <carloshenriquegrandi2@gamil.com>
2024-03-21 09:54:29 -0300

    “Plotando múltiplas linhas no gráfico para múltiplos conjuntos de dados

--- geragrafico.py
+++ geragrafico.py
@@ -1,28 +1,33 @@
 
-from leitorarquivo import leitorarquivo
+from leitorarquivo import LeitorArquivo
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def main():
-    leitor = leitorarquivo('data.txt')
+    leitor = LeitorArquivo('data.txt')
     valores = leitor.getValores()
     print(valores)
 
+    i = 1
+    for serie in valores:
+        plt.plot(serie, label='Série ' + str(i))   
+        i += 1
+    plt.legend(loc='upper left')
+
+
     plt.ylabel('Valores de entrada')
     plt.xlabel('Amostragem')
-
     plt.title('Gráfico de linhas')
-  i = 1
- for serie in valores:
-   plt.plot(serie, label='Série ' + str(i))   
-   i += 1
-
-
-
-  plt.legend(loc='upper left')
 
     plt.show()
 
-
 main()
+
+
+
+       
+
+
+
 
